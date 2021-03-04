57601c0894ad04983663b37baafa85725c87c019
dangolbeeker <scottb305@outlook.com>
2021-03-04 10:37:36 -0500

    Spring Boot Kotlin init

---
+++ gQLK/src/main/kotlin/com/gQLK/GQlkApplication.kt
@@ -0,0 +1,11 @@
+package com.gQLK
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GQlkApplication
+
+fun main(args: Array<String>) {
+	runApplication<GQlkApplication>(*args)
+}
---
+++ gQLK/src/test/kotlin/com/gQLK/GQlkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gQLK
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GQlkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
